a010b4b74c64839a261b92cd5dfd0c7f1ae15855
giovanna imlau <imlaugi@gmail.com>
2024-03-31 21:50:55 -0300

    segundo commit do js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-alert("hello word")
+let conta = 0
+let pessoas = 0
+let porcentagem = 0
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta)
+
+function receberValorConta(evento) {
+    conta = Number(evento.target.value)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+function receberQuantidadePessoas(evento) {
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+
+    if(evento.target.value === "0") {
+        paragrafoErro.computedStyleMap.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    } else {
+        paragrafoErro.computedStyleMap.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas =  Number(evento.target.value)
+    }
+}
+
+const botoesGorjeta = document.querySelectorALL(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao =>{
+    botao.addEventListener("click", receberPorcentagem)
+})
+
+function receberPorcentagem(evento) {
+    console.log(evento.target.value)
+}
